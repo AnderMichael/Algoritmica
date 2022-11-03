2f1a72b0a10fe7973cc4e418d82f85c9dd6df50e
Ander Michael Cayllan Mamani <101998948+AnderMichael@users.noreply.github.com>
2022-11-02 23:21:57 -0400

    Create CoinsAndTriangles.cpp

---
+++ Teoría_Números/Búsqueda_Binaria/Problemas/CoinsAndTriangles.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
+using namespace std;
+int t, m;
+bool f(int n)
+{
+    int res = (n + 1) * n / 2;
+    if (res > m)
+        return false;
+    else
+        return true;
+}
+
+int binarySearch(int left, int right)
+{
+    int mid = (left + right) / 2;
+    if (left == right || right < left)
+    {
+        if (f(mid))
+            return mid;
+        else
+            return mid - 1;
+    }
+    else
+    {
+        if (f(mid))
+            return binarySearch(mid + 1, right);
+        else
+            return binarySearch(left, mid - 1);
+    }
+}
+
+int main()
+{
+    input;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &m);
+        printf("%d\n", binarySearch(1, min(m, 44720)));
+    }
+}
